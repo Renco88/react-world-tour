b097c44cc41fe475b24e7c769afe3d1c6d0392f1
renco88 <mdmahadihassanrenco@gmail.com>
2024-10-25 22:34:01 +0600

    add going and visited button

--- src/component/Country.jsx
+++ src/component/Country.jsx
@@ -1,13 +1,27 @@
-import './Country.css';
-const Country = ({country}) => {
-   const{name, flags} = country;
-    return (
-        <div className="country">
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt="" />
-            
-        </div>
-    );
+import { useState } from "react";
+import "./Country.css";
+const Country = ({ country }) => {
+  const { name, flags, population, area, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
+  return (
+    <div className="country">
+      <h3>Name: {name?.common}</h3>
+      <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      <p>
+        <small>Coad: {cca3}</small>
+      </p>
+      <button onClick={handleVisited}>{!visited ? "Visited" : "Going"}</button>
+      {visited ? "I Have Visited This Country" : "I Want To Visited"}
+    </div>
+  );
 };
 
 export default Country;
